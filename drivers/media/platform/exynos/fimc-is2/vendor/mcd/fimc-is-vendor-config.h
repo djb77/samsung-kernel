4e203bfbbf4fee9d4442da2501da580375d07667
djb77 <dwayne.bakewell@gmail.com>
2017-11-11 19:35:06 +1100

    OSRC: Samsung G93XFXXU1DQE5 Kernel Update
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/media/platform/exynos/fimc-is2/vendor/mcd/fimc-is-vendor-config.h
+++ drivers/media/platform/exynos/fimc-is2/vendor/mcd/fimc-is-vendor-config.h
@@ -17,10 +17,6 @@
 #include "fimc-is-vendor-config_hero.h"
 #elif defined(CONFIG_CAMERA_GRACE)
 #include "fimc-is-vendor-config_grace.h"
-#elif defined(CONFIG_CAMERA_VJFLTE)
-#include "fimc-is-vendor-config_vjflte.h"
-#elif defined(CONFIG_CAMERA_ULTE)
-#include "fimc-is-vendor-config_ulte.h"
 #else
 #include "fimc-is-vendor-config_hero.h" /* Default */
 #endif
