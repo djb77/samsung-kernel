0e2f66e39509a9cc31932d9fcdf09a1b7c6f1e6d
djb77 <dwayne.bakewell@gmail.com>
2018-08-28 17:44:29 +1000

    OSRC: Samsung G93XFXXU2ERH1 Kernel Update
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ARCH=arm64
+export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export ANDROID_MAJOR_VERSION=o
 
 make exynos8890-herolte_defconfig
-make -j
+make -j64
